049731ccf8d3547ecaf48d764140abd0cee3c9ce
risma797 <rismanurhayati.g@gmail.com>
2021-06-03 20:58:11 +0700

    binnary search

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import models.Word;
+
+
+public class Main {
+
+
+    static void printWord(Word word, String keyWord) {
+        System.out.println("Kata '" + keyWord + "' Ditemukan");
+        System.out.println("English : " + word.english);
+        System.out.println("Indonesia : " + word.indonesia);
+    }
+
+    public static void main(String[] args) {
+
+        BinnarySearch search = new BinnarySearch();
+
+        search.addSeed(new Word("belajar", "learn"));
+        search.addSeed(new Word("mimpi", "dream"));
+        search.addSeed(new Word("latihan", "practice"));
+        search.addSeed(new Word("kejar", "chase"));
+        search.addSeed(new Word("pelajaran", "lesson"));
+        search.addSeed(new Word("soal", "question"));
+
+
+        /**
+         * Search from english to indonesia
+         */
+
+        System.out.println("\n====== Cari berdasarkan kata inggris ======");
+
+        String keyWord1 = "chase";
+        Word customSearch1 = search.searchFromEnglish(0, search.words.size(), keyWord1);
+
+        if(customSearch1 != null) {
+            printWord(customSearch1, keyWord1);
+        } else {
+            System.out.println("Kata '"+ keyWord1 +"' Tidak Ditemukan");
+        }
+
+        /**
+         * Search from indonesia to english
+         */
+
+        System.out.println("\n====== Cari berdasarkan kata indonesia ======");
+
+        String keyWord2 = "kejar";
+        Word customSearch2 = search.searchFromIndonesia(0, search.words.size(), keyWord2);
+
+        if(customSearch2 != null) {
+            printWord(customSearch2, keyWord2);
+        } else {
+            System.out.println("Kata '"+ keyWord2 +"' Tidak Ditemukan");
+        }
+
+
+        /**
+         * Search from indonesia to english when notfound
+         */
+
+        System.out.println("\n====== Cari berdasarkan kata indonesia [not found] ======");
+
+        String keyWord3 = "kejar-mimpi";
+        Word customSearch3 = search.searchFromIndonesia(0, search.words.size(), keyWord3);
+
+        if(customSearch3 != null) {
+            printWord(customSearch3, keyWord3);
+        } else {
+            System.out.println("Kata '"+ keyWord3 +"' Tidak Ditemukan");
+        }
+
+    }
+}
